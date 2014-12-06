70afde73997238254da675328589e476ae59185a
Scaffale <Martino.giacchetti@gmail.com>
2014-12-06 09:27:58 +0100

    Initial Commit

---
+++ Splatter/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Splatter
+//
+//  Created by Martino Giacchetti on 06/12/14.
+//  Copyright (c) 2014 HCI_assignment. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
